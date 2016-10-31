a0909932bfbaebc0a8f842bfd8fe873ff2d395b7
Lucia Espona <lucia.espona@wsl.ch>
2016-10-31 15:34:38 +0100

    added citation display

---
+++ ckanext/envidat_schema/helpers.py
@@ -0,0 +1,77 @@
+import json
+
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+def envidat_schema_get_citation(package_data_dict):
+    '''
+    Combines multiple datacite metadata into a
+    citation string. Pattern:
+    creators (publication_year): title; subtitle. publisher; doi:identifier.
+    '''
+    citation = u''
+
+    # creators
+    creators = _get_from_json_dict_list(package_data_dict.get('author', ""), 'name')
+    if creators:
+        citation += creators
+
+    # publication_year
+    publication_year = _get_from_json_dict(package_data_dict.get('publication', ""), 'publication_year')
+    if (publication_year):
+        citation += ' ('+ publication_year + ')'
+
+    # separator
+    if citation:
+        citation +=': '
+
+    # title
+    title = package_data_dict.get('title', "")
+    if title:
+        citation += title.strip() + '; '
+
+    # publisher
+    publisher = _get_from_json_dict(package_data_dict.get('publication', ""), 'publisher')
+    if (publisher):
+        citation += publisher + '; '
+
+    # doi:identifier.
+    doi = package_data_dict.get('doi', "")
+    if doi:
+        citation += 'doi:' + doi.strip() + '.'
+
+    return citation
+
+
+def _get_from_json_dict (text, field):
+    try:
+        json_dict = json.loads(text)
+        value = json_dict.get(field, "")
+    except e:
+        return ""
+    return _safe_str(value).strip()
+
+def _get_from_json_dict_list (text, field, sep='; '):
+    try:
+        json_list = json.loads(text)
+        if type(json_list) is not list:
+            json_list = [ json_list ]
+        value_list = []
+        for item in json_list:
+            value = _safe_str(item.get(field, "")).strip()
+            if value:
+                value_list += [ value ]
+    except e:
+        return ""
+    return (sep.join(value_list))
+
+
+def _safe_str(obj):
+    """ return the byte string representation of obj """
+    try:
+        return str(obj)
+    except UnicodeEncodeError:
+        # obj is unicode
+        return unicode(obj)
--- ckanext/envidat_schema/plugin.py
+++ ckanext/envidat_schema/plugin.py
@@ -1,13 +1,18 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+from ckanext.envidat_schema import helpers
 
 class Envidat_SchemaPlugin(plugins.SingletonPlugin):
-     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.ITemplateHelpers)
 
-     # IConfigurer
+    # IConfigurer
 
-     def update_config(self, config_):
-         toolkit.add_template_directory(config_, 'templates')
-         toolkit.add_public_directory(config_, 'public')
-         toolkit.add_resource('fanstatic', 'envidat_schema')
+    def update_config(self, config_):
+        toolkit.add_template_directory(config_, 'templates')
+        toolkit.add_public_directory(config_, 'public')
+        toolkit.add_resource('fanstatic', 'envidat_schema')
 
+    # ITemplateHelpers
+    def get_helpers(self):
+	return { 'envidat_schema_get_citation': helpers.envidat_schema_get_citation }
